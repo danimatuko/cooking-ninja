f31f389b369b3c98c642f8505eb185c0742a38d8
Matuko <matuko@Dani-Matuko.lan>
2022-07-26 20:45:32 +0300

    Router v6 + files structure

--- cooking-ninja/src/App.js
+++ cooking-ninja/src/App.js
@@ -1,11 +1,23 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ cooking-ninja/src/pages/recipe/Recipe.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./recipe.css"
+
+const Recipe = () => {
+  return (
+    <div>Recipe</div>
+  )
+}
+
+export default Recipe
