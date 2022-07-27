3d7eb1e14b2e754af5fd2cefc719be2a77524dd1
Matuko <matuko@Dani-Matuko.lan>
2022-07-27 08:36:32 +0300

    add ingredients form field

--- cooking-ninja/src/pages/create/Create.jsx
+++ cooking-ninja/src/pages/create/Create.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Create.css";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [method, setMethod] = useState("");
   const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime);
+    console.log({ title, method, cookingTime, ingredients });
+  };
+
+  const addIngredient = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing]);
+    }
+    setNewIngredient("");
+    ingredientInput.current.focus();
   };
 
   return (
@@ -23,8 +37,28 @@ const Create = () => {
             value={title}
             required
           />
+        </label>{" "}
+        <label>
+          <span>Recipe ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button className="btn" onClick={addIngredient}>
+              add
+            </button>
+          </div>
         </label>
         <label>
+          <p>
+            Current Ingredients:{" "}
+            {ingredients.map((ing) => (
+              <em key={ing}>{ing}, </em>
+            ))}
+          </p>
           <span>Recipe Method:</span>
           <textarea
             type="text"
