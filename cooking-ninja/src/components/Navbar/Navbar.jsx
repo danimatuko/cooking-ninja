fb6df928388935e30f93f9cfca42ec68a908aedc
Matuko <matuko@Dani-Matuko.lan>
2022-07-30 20:13:02 +0300

    Add theme selector

--- cooking-ninja/src/components/Navbar/Navbar.jsx
+++ cooking-ninja/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className="navbar" style={{ background: color }}>
-      <nav onClick={() => changeColor("pink")}>
+      <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
