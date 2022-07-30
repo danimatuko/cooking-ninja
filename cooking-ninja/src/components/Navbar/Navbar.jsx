36ebc0e891b17b9d6085fff1097267f943032166
Matuko <matuko@Dani-Matuko.lan>
2022-07-30 18:36:19 +0300

    add themContext

--- cooking-ninja/src/components/Navbar/Navbar.jsx
+++ cooking-ninja/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../../context/ThemeContext.js";
 import Search from "../SearchBar/SearchBar";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const { color } = useContext(ThemeContext);
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
